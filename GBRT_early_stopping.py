2c41ef279a43f2889d7cb58130034fe8001d70de
Vonkondok <50097507+Vonkondok@users.noreply.github.com>
2020-02-20 15:07:30 +0800

    Update GBRT_early_stopping.py

--- GBRT_early_stopping.py
+++ GBRT_early_stopping.py
@@ -1,11 +1,12 @@
 from sklearn.ensemble import GradientBoostingRegressor
+
 gbrt = GradientBoostingRegressor(max_depth=2, warm_start=True)
 min_val_error = float("inf")
 error_going_up = 0
 
 for n_estimators in range(1, 120):
-	gbrt.n_estimators = n_estimators
-	gbrt.fit(X_train, y_train)
+    gbrt.n_estimators = n_estimators
+    gbrt.fit(X_train, y_train)
 	y_pred = gbrt.predict(X_val)
 	val_error = mean_squared_error(y_val, y_pred)
 	if val_error < min_val_error:
