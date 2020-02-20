732a10b4cc50fe9e1068b429c8a0fb7b276398b5
Vonkondok <50097507+Vonkondok@users.noreply.github.com>
2020-02-20 15:08:37 +0800

    Update GBRT_early_stopping.py

--- GBRT_early_stopping.py
+++ GBRT_early_stopping.py
@@ -7,12 +7,12 @@ error_going_up = 0
 for n_estimators in range(1, 120):
     gbrt.n_estimators = n_estimators
     gbrt.fit(X_train, y_train)
-	y_pred = gbrt.predict(X_val)
-	val_error = mean_squared_error(y_val, y_pred)
-	if val_error < min_val_error:
-		min_val_error = val_error
-		error_going_up = 0
-	else:
-		error_going_up += 1
-		if error_going_up == 5:
-			break # early stopping	
+    y_pred = gbrt.predict(X_val)
+    val_error = mean_squared_error(y_val, y_pred)
+    if val_error < min_val_error:
+        min_val_error = val_error
+    error_going_up = 0
+    else:
+        error_going_up += 1
+        if error_going_up == 5:
+            break # early stopping	
